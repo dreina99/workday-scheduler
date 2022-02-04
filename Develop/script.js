447eef35ccf6241a42e7f32f5d6344f63499bcf0
devin <devinreina99@gmail.com>
2022-02-04 12:46:34 -0800

    added time-based color coding/local storage for tasks

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,36 @@
 var currentDate = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentDate);
 
-//console.log(currentDate);
+var hour = moment().hour();
+console.log(hour);
+
+$(".saveBtn").click(function(e) {
+    var divID = $(this).parents("div").attr("id");
+    //console.log(divID);
+    var textAreaContent = $(this).siblings(".content").val();
+    //console.log(textAreaContent);
+    localStorage.setItem(divID, textAreaContent);
+});
+
+var loadArray = document.querySelectorAll(".content");
+for(let i = 0; i < loadArray.length; i++)
+{
+    loadArray[i].innerHTML = localStorage.getItem(i + 9);
+}
+
+var timeBlock = document.querySelectorAll(".row");
+for(let i = 0; i < timeBlock.length; i++)
+{
+    if(hour < i + 9)
+    {
+        timeBlock[i].classList.add("future");
+    }
+    else if(hour > i + 9)
+    {
+        timeBlock[i].classList.add("past");
+    }
+    else
+    {
+        timeBlock[i].classList.add("present");
+    }
+}
